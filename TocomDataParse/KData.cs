1b50ce8ea29bcd891e05ed8b9334371e2129c267
dada2cindy <dada2cindy@gmail.com>
2012-07-06 06:38:57 +0000

    第一版trunk

---
+++ TocomDataParse/KData.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TocomDataParse
+{
+    public class KData
+    {
+        /// <summary>
+        /// 日期 YYYYMMDD
+        /// </summary>
+        public virtual string Date { get; set; }
+
+        /// <summary>
+        /// Trading method
+        /// </summary>
+        public virtual string TradingMethod { get; set; }
+
+        /// <summary>
+        /// Commodity
+        /// </summary>
+        public virtual string Commodity { get; set; }
+
+        /// <summary>
+        /// ContractMonth YYYYMM
+        /// </summary>
+        public virtual string ContractMonth { get; set; }
+
+        /// <summary>
+        /// StrikePrice
+        /// </summary>
+        public virtual string StrikePrice { get; set; }
+
+        /// <summary>
+        /// OpeningPrice
+        /// </summary>
+        public virtual string OpeningPrice { get; set; }
+
+        /// <summary>
+        /// HighestPrice
+        /// </summary>
+        public virtual string HighestPrice { get; set; }
+
+        /// <summary>
+        /// LowestPrice
+        /// </summary>
+        public virtual string LowestPrice { get; set; }
+
+        /// <summary>
+        /// ClosingPrice
+        /// </summary>
+        public virtual string ClosingPrice { get; set; }
+
+        /// <summary>
+        /// SettlementPrice
+        /// </summary>
+        public virtual string SettlementPrice { get; set; }
+
+        /// <summary>
+        /// TradingVolume
+        /// </summary>
+        public virtual string TradingVolume { get; set; }
+
+        /// <summary>
+        /// OpenInterest
+        /// </summary>
+        public virtual string OpenInterest { get; set; }
+    }
+}
